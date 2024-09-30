9334e18996de5b8fc942da2d8afd37526b2291d1
Mugisha Gentil <gentilmugisha178@gmail.com>
2024-09-30 09:36:28 +0200

    My-First-Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Mugisha Gentil</h1>
+     <h1>Byiringiro Urban</h1>
+     <h2>Cyussa Stessy</h2>
+    </div>
+  );
+}
+
+export default App;
